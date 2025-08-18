8fcd62f09e394dbfdc4e8a333a1d905158d177bb
HanChaeYeong <hcy59188@gmail.com>
2025-08-19 02:46:57 +0900

    mon

--- graph/단지번호붙이기.cpp
+++ graph/단지번호붙이기.cpp
@@ -8,20 +8,19 @@ int N;
 vector<vector<int>> map;
 vector<vector<bool>> visited;
 vector<int> house;
-int cnt=0;
 
 int dfs(int v1, int v2){
     visited[v1][v2] = true;
-    if(map[v1][v2]) cnt++;
+    int cnt=1;
     //상 하 좌 우
     if (v1-1 >= 0 && !visited[v1-1][v2] && map[v1-1][v2]) 
-        dfs(v1-1, v2); 
+        cnt += dfs(v1-1, v2); 
     if (v1+1 < N && !visited[v1+1][v2] && map[v1+1][v2]) 
-        dfs(v1+1, v2);
+        cnt += dfs(v1+1, v2);
     if (v2-1 >= 0 && !visited[v1][v2-1] && map[v1][v2-1]) 
-        dfs(v1, v2-1);
+        cnt += dfs(v1, v2-1);
     if (v2+1 < N && !visited[v1][v2+1] && map[v1][v2+1]) 
-        dfs(v1, v2+1);
+        cnt += dfs(v1, v2+1);
 
     return cnt;
 }
@@ -35,9 +34,7 @@ int main(){
     string line;
     for(int i=0; i<N; i++){
         cin>>line;
-        for(int j=0; j<line.length(); j++){
-            map[i].push_back(line[j]-'0');
-        }
+        for(char c: line) map[i].push_back(c-'0');
     }
 
     for(int i=0; i<map.size(); i++){
@@ -45,10 +42,10 @@ int main(){
             if(map[i][j] && !visited[i][j]) {
                 int ans = dfs(i, j);
                 house.push_back(ans);
-                cnt=0;
             }
         }
     }
+    sort(house.begin(), house.end());
     cout<<house.size()<<'\n';
     for(int x: house) cout<<x<<'\n';
 }
