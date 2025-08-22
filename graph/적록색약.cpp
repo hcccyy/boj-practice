f0002d0ae5d3690555d2d8b4bb9ccb9b305bbc59
HanChaeYeong <hcy59188@gmail.com>
2025-08-22 14:42:32 +0900

    df

--- graph/적록색약.cpp
+++ graph/적록색약.cpp
@@ -8,34 +8,25 @@ vector<vector<char>> paint;
 vector<vector<bool>> visited;
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
+bool flag=false;
 
-void color(int x, int y, char c){
+void dfs(int x, int y, char c){
     visited[x][y] = true;
     int nx, ny;
     for(int i=0; i<4; i++){
         nx = dx[i] + x;
         ny = dy[i] + y;
-        if(nx>=0 && nx<N && ny>=0 && ny<N){
-            if(!visited[nx][ny] && c == paint[nx][ny]) 
-                color(nx, ny, c);
-        }
-    }
-}
-
-void nonColor(int x, int y, char c){
-    visited[x][y] = true;
-    int nx, ny;
-
-    for(int i=0; i<4; i++){
-        nx = dx[i] + x;
-        ny = dy[i] + y;
-        
         if(nx>=0 && nx<N && ny>=0 && ny<N){
             if(!visited[nx][ny]){
-                if((c=='R' || c=='G') && (paint[nx][ny]=='R' || paint[nx][ny]=='G'))
-                    nonColor(nx, ny, c);
-                else if(c == paint[nx][ny])
-                    nonColor(nx, ny, c);
+                if(flag){
+                    if((c=='R' || c=='G') && (paint[nx][ny]=='R' || paint[nx][ny]=='G'))
+                        dfs(nx, ny, c);
+                    else if(c == paint[nx][ny])
+                        dfs(nx, ny, c);
+                }
+                else {
+                    if(c == paint[nx][ny]) dfs(nx, ny, c);
+                }
             }
         }
     }
@@ -53,22 +44,20 @@ int main(){
     for(int i=0; i<N; i++){
         for(int j=0; j<N; j++) {
             if(!visited[i][j]) {
-                color(i, j, paint[i][j]);
+                dfs(i, j, paint[i][j]);
                 cnt++; 
             }
         }
     }
     cout<<cnt<<' ';
     //reset
-    cnt=0;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++) visited[i][j]=false;
-    }
+    cnt=0; flag = true;
+    visited.assign(N, vector<bool>(N, false));
 
     for(int i=0; i<N; i++){
         for(int j=0; j<N; j++) {
             if(!visited[i][j]) {
-                nonColor(i, j, paint[i][j]);
+                dfs(i, j, paint[i][j]);
                 cnt++; 
             }
         }
